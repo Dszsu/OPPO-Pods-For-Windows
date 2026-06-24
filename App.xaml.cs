8f4b264fb3e98da8e4bf98b2d62ca2c29ca85757
Zhao-Yi <zhaoyi@users.noreply.github.com>
2026-06-24 18:42:10 +0800

    启动时跟随系统深浅色主题

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,7 +1,13 @@
 using System.Windows;
+using Wpf.Ui.Appearance;
 
 namespace OppoPodsWPF;
 
 public partial class App : Application
 {
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+        ApplicationThemeManager.ApplySystemTheme();
+    }
 }
